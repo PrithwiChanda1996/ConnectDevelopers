61ea01af5ff493a1811083860224e0ff15afd7ef
PithwiChanda <59953120+PithwiChanda@users.noreply.github.com>
2021-08-19 21:19:49 +0530

    Logout implemented

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   LOGIN_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -94,4 +95,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / clear profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
