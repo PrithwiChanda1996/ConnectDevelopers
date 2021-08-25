4c2b68f707a5a065ae1926c788a79744fa3f6711
PithwiChanda <59953120+PithwiChanda@users.noreply.github.com>
2021-08-25 15:51:24 +0530

    Add experience and education is implemented

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles">
+          <i className="fas fa-users"></i>{" "}
+          <span className="hide-sm">Developers</span>
+        </Link>
       </li>
       <li>
         <Link to="/dashboard">
