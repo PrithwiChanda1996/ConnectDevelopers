258deea88183dfaead8a79afd89fdd2d933a4b5e
PithwiChanda <59953120+PithwiChanda@users.noreply.github.com>
2021-08-31 21:59:09 +0530

    add and remove like implemented

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,5 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import { updateLocale } from "moment";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -21,6 +22,14 @@ export default function (state = initialState, action) {
       return {
         ...state,
         error: payload,
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false,
       };
     default:
